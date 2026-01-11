17e862f7793f50af56b1a8af631f6d9f005693f8
safakferhatkaya <safakferhatkaya@gmail.com>
2026-01-11 16:33:22 +0300

    Create initial app with mock data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module arb-hunter
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type Exchane interface {
+	Name() string
+	GetPrice(symbol string) (float64, error)
+}
+
+// Implementation of binance
+type Binance struct{}
+
+func (b Binance) Name() string { return "Binance" }
+func (b Binance) GetPrice(symbol string) (float64, error) {
+	// Todo: Implement actual API call
+	return 98.000, nil
+}
+
+type Coinbase struct{}
+func (k Coinbase) Name() string { return "Coinbase" }
+func (k Coinbase) GetPrice(symbol string) (float64, error) {
+	// Todo: Implement actual API call
+	return 98.500, nil
+}
+
+func main() {
+	exchanges := []Exchane{
+		Binance{},
+		Coinbase{},
+	}
+
+	symbol := "BTC"
+	fmt.Printf("Searching prices for %s:\n", symbol)
+
+	// Todo : Implement concurrency for fetching prices
+	for _, ex := range exchanges {
+		price, err := ex.GetPrice(symbol)
+		if err != nil {
+			log.Println("Error:", err)
+			continue
+		}
+		fmt.Printf("[%s] Price $%.2f\n", ex.Name(), price)
+	}
+}
